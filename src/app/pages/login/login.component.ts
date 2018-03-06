3c07333c0b5431149fd946bca242310150896a1e
faisal amdani <30832544+amdani786@users.noreply.github.com>
2018-03-06 12:43:55 +0530

    Add files via upload

---
+++ src/app/pages/login/login.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+
+
+
+
+
+
+@Component({
+
+  styleUrls: ['./login.component.scss'],
+  templateUrl: './login.component.html',
+})
+
+
+export class LoginComponent {
+
+
+
+
+
+ constructor() {}
+ 
+ }
+ 
+ 
---
+++ src/app/pages/login/login.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+
+
+
+import { LoginComponent } from './login.component';
+
+
+@NgModule({
+  imports: [
+
+  ],
+  declarations: [
+    LoginComponent,
+       
+  ],
+})
+export class LoginModule { }
